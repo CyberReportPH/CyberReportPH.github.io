cac61665e524bf04cdb2c6b2a884cb52aa868705
Anton Suba <antonsuba@outlook.com>
2017-04-02 15:15:59 +0800

    Implement form frontend

--- js/app.js
+++ js/app.js
@@ -1,7 +1,21 @@
+let cyberCrimeType = '';
+let name = '';
+let email = '';
+let address = '';
+let birthDate = '';
+let contacNumber = '';
+let details = '';
+let appointmentTime = '';
+
 function clicked(button){
     let selectedBtn = document.querySelector('.clicked');
     if(selectedBtn){
         selectedBtn.classList.remove('clicked');
     }
+    cyberCrimeType = button.textContent || button.innerText;
     button.classList.add('clicked');
+}
+
+function submitForm(){
+
 }
