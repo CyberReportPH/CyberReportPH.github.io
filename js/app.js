2efcb218b7f7bb04d09073bbd179a79285f9d191
Anton Suba <antonsuba@outlook.com>
2017-04-02 17:03:56 +0800

    Implement structure for appointments

--- js/app.js
+++ js/app.js
@@ -16,6 +16,40 @@ function clicked(button){
     button.classList.add('clicked');
 }
 
-function submitForm(){
+function validateForm(form){
+    let limit = form.length;
+    let failed = false;
+    for(let i = 0; i < limit; i++){
+        if(!form[i].value){
+            form[i].classList.add('uk-form-danger');
+            failed = true;
+        }
+    }
+    if(failed){
+        return false;
+    }
+    return true;
+}
 
+function addPersonalInfo(){
+    let form = document.querySelector('#personal-info');
+
+    if(!validateForm(form)){
+        console.log("Fail");
+        return;
+    }
+
+    console.log("Success");
+
+    let elements = form.elements;
+    name = elements['last-name'].value + ', ' + elements['first-name'].value + ' ' + elements['middle-initial'].value + '.';
+    email = elements['email'].value;
+    address = elements['address'].value;
+    birthDate = elements['birth-month'].value + ' ' + elements['birth-date'].value + ' ' + elements['birth-year'].value;
+    contacNumber = elements['contact-number'].value;
+    details = elements['crime-details'].value;
+}
+
+function submitReport(){
+    
 }
