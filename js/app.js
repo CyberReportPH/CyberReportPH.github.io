abdf019137d12e9799f541964c489dd214e9d403
Anton Suba <antonsuba@outlook.com>
2017-04-02 19:05:51 +0800

    Complete reporting process structure

--- js/app.js
+++ js/app.js
@@ -5,15 +5,28 @@ let address = '';
 let birthDate = '';
 let contacNumber = '';
 let details = '';
-let appointmentTime = '';
+let appointmentDate = '';
+
+function changeSection(currentSection, nextSection){
+    document.getElementById(currentSection).classList.add('hide');
+    document.getElementById(nextSection).classList.remove('hide');
+}
 
 function clicked(button){
     let selectedBtn = document.querySelector('.clicked');
     if(selectedBtn){
         selectedBtn.classList.remove('clicked');
     }
-    cyberCrimeType = button.textContent || button.innerText;
     button.classList.add('clicked');
+}
+
+function addCrimeType(){
+    let selectedBtn = document.querySelector('.clicked');
+    if(!selectedBtn){
+        return;
+    }
+    cyberCrimeType = selectedBtn.textContent || selectedBtn.innerText;
+    changeSection('crime-types', 'personal-info')
 }
 
 function validateForm(form){
@@ -32,24 +45,60 @@ function validateForm(form){
 }
 
 function addPersonalInfo(){
-    let form = document.querySelector('#personal-info');
+    let form = document.querySelector('#personal-info-form');
 
     if(!validateForm(form)){
         console.log("Fail");
-        return;
+        //return;
     }
 
     console.log("Success");
 
     let elements = form.elements;
+
     name = elements['last-name'].value + ', ' + elements['first-name'].value + ' ' + elements['middle-initial'].value + '.';
     email = elements['email'].value;
     address = elements['address'].value;
     birthDate = elements['birth-month'].value + ' ' + elements['birth-date'].value + ' ' + elements['birth-year'].value;
     contacNumber = elements['contact-number'].value;
     details = elements['crime-details'].value;
+
+    changeSection('personal-info', 'appointments');
+}
+
+function addTime(){
+    let time= document.querySelector('input[name="timePicker"]:checked').value;
+
+    console.log(time)
+    if(!time){
+        return false;
+    }
+
+    appointmentDate = '4/2/2017'; //Sample Date -> Change soon
+    appointmentTime = time;
+
+    return true;
 }
 
 function submitReport(){
     
+    if(!addTime()){
+        return;
+    }
+
+    console.log('prepare send');
+
+    emailjs.send('gmail', 'cyberreportph', {
+        to_email: email,
+        name: name, 
+        date: appointmentDate,
+        time: appointmentTime,
+        address: address,
+        details: details
+    }).then(response => {
+        console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
+        changeSection('appointments', 'thank-you');
+    }).catch(error => {
+        console.log(error);
+    });
 }
